5afb729f5140c79d08f89a07ed6bcc87f13f59c7
root <root@test.(none)>
2014-05-14 01:22:25 -0400

    tournamentjs file

--- app/views/tournament.php
+++ app/views/tournament.php
@@ -81,9 +81,8 @@
                   ?>
               </div>
           </div>
-          <div class="row">
             <div class="col-md-4">
-                 <h2>Information</h2>
+                 <h2>Control Panel</h2>
                  <h4>Live URL:</h4> Coming soon <br />
             </div>
       </div>
